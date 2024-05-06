bcf9626680d8572ce8644c56874e1dc9673c46bf
brayo <vukubrian@gmail.com>
2024-05-06 22:04:47 +0300

    chore: cargo fmt

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,8 +1,8 @@
 fn main() {
-  let webui_var = std::env::var("AW_WEBUI_DIR");
-  
-  if let Err(_) = webui_var {
-    panic!("AW_WEBUI_DIR environment variable not set, Try running make");
-  }
-  tauri_build::build();
+    let webui_var = std::env::var("AW_WEBUI_DIR");
+
+    if let Err(_) = webui_var {
+        panic!("AW_WEBUI_DIR environment variable not set, Try running make");
+    }
+    tauri_build::build();
 }
