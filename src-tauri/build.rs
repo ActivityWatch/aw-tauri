73f80e36d4789ecc67819ce9e972b77f1032b357
Erik Bjäreholt <erik@bjareho.lt>
2024-05-13 12:42:55 +0200

    fix: added `cargo:rerun-if-changed` for webui path to build.rs

--- src-tauri/build.rs
+++ src-tauri/build.rs
@@ -1,8 +1,12 @@
 fn main() {
     let webui_var = std::env::var("AW_WEBUI_DIR");
-
-    if let Err(_) = webui_var {
+    if webui_var.is_err() {
         panic!("AW_WEBUI_DIR environment variable not set, Try running make");
     }
+
+    // Rebuild if the webui directory changes
+    println!("cargo:rerun-if-changed={}", webui_var.unwrap());
+    println!("cargo:rerun-if-env-changed=AW_WEBUI_DIR");
+
     tauri_build::build();
 }
