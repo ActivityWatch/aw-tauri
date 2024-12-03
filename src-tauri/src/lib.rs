a195e9b38a8f54ae201ca985566dee1bc1b73ff6
brayo <vukubrian@gmail.com>
2024-12-03 16:29:00 +0300

    feat: restart on module crash + show dialog
    chore: mark dialog as done

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -44,6 +44,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_autostart::init(
             MacosLauncher::LaunchAgent,
             Some(vec![]),
