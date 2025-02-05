4b65c5a7e2b92fa9e2b0662de8fbb57e5a5b7bf7
Brayo <vukubrian@gmail.com>
2025-02-05 11:24:32 +0300

    chore: add http plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -308,6 +308,7 @@ pub fn run() {
     }
 
     tauri::Builder::default()
+        .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_dialog::init())
